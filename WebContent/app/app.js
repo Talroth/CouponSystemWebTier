0817fb33a3148814f2810ede780ebc620a095cdb
Talroth <tal7423775@yahoo.com>
2016-11-12 03:40:24 +0200

    Copy mysql driver, jersey media and mimepull jars to WebContent/WEB-INF
    update web.xml with classpath to media

--- WebContent/app/app.js
+++ WebContent/app/app.js
@@ -1,21 +1,18 @@
-/**
- * 
- */
-/*global angular */
+
 var app = angular.module('login', ['ngMaterial']);
 app.controller("loginController", ['$scope', '$http', '$window', '$location', function ($scope, $http, $window, $location) {
 
 $scope.login = function() {
     $http({
-         url: 'http://localhost:8080/CouponSystemWebTier/rest/customerService/login?user=Bill Joe&password=bj',  
-       //  url: 'http://localhost:8080/JerseyTrial/rest/adminService/login?user=admin&password=1234',      
-        //  url: 'http://localhost:8080/JerseyTrial/rest/companyService/login?user=McHoland&password=f12e12',   
+       //  url: 'http://localhost:8080/CouponSystemWebTier/rest/customerService/login?user=Bill Joe&password=bj',  
+       //  url: 'http://localhost:8080/CouponSystemWebTier/rest/adminService/login?user=admin&password=1234',      
+          url: 'http://localhost:8080/CouponSystemWebTier/rest/companyService/login?user=McHoland&password=f12e12',   
         method: 'POST'
     }).success(function(response){
         $scope.test = response;
-        $window.location.href = "http://" + $location.host() + ":" + $location.port() + "/CouponSystemWebTier/views/customer.html";  
-       // $window.location.href = "http://" + $location.host() + ":" + $location.port() + "/JerseyTrial/views/admin.html";     
-       //   $window.location.href = "http://" + $location.host() + ":" + $location.port() + "/JerseyTrial/views/company.html";   
+       // $window.location.href = "http://" + $location.host() + ":" + $location.port() + "/CouponSystemWebTier/views/customer.html";  
+       // $window.location.href = "http://" + $location.host() + ":" + $location.port() + "/CouponSystemWebTier/views/admin.html";     
+          $window.location.href = "http://" + $location.host() + ":" + $location.port() + "/CouponSystemWebTier/views/company.html";   
         /* "/customer.html"; */
     })
         .error(function(response){console.log("error occurred."); 
@@ -24,7 +21,7 @@ $scope.login = function() {
 
 $scope.logout = function() {
     $http({
-        url: 'http://localhost:8080/CouponSystemWebTier/rest/customerService/logout',
+        url: "http://" + $location.host() + ":" + $location.port() + "/CouponSystemWebTier/rest/customerService/logout",
         method: 'POST'
     }).success(function(response) {
         $scope.test = response;
