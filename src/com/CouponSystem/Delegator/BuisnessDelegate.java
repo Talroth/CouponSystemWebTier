a03b136cec323269ce48815530943f12cf282477
jbt <jbt@302_13>
2016-11-13 21:18:11 +0200

    Delgate with the updated file

---
+++ src/com/CouponSystem/Delegator/BuisnessDelegate.java
@@ -0,0 +1,29 @@
+package com.CouponSystem.Delegator;
+
+import java.util.Collection;
+
+import com.CouponSystem.Beans.Income;
+
+public class BuisnessDelegate {
+	
+	public void storeIncome(Income income)
+	{
+		
+	}
+	
+	public void viewIncomeByCompany(long companyId)
+	{
+		
+	}
+	
+	public void viewAllIncomes()
+	{
+		
+	}
+	
+	public void viewIncomeByCustomer(long customerId)
+	{
+		
+	}
+
+}
